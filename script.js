55e52d62562c03c2c2a4300bb0768857d9ea0023
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-08-14 18:12:09 -0400

    Added event listener to open modal on button click. Removed uneccessary styling in CSS.

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
-/* const myLibrary = [
-  { title: "State of Fear", author: "Michael Crichton", pages: 603 },
-  { title: "Wool", author: "Hugh Howey", pages: 592 },
-  { title: "None of This is True", author: "Lisa Jewell", pages: 384 },
-];
+let myLibrary = [];
 
-function addBookToLibrary(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-}
+const addBookBtn = document.getElementById("addBook");
+const modal = document.querySelector(".modal");
+const submitBtn = document.querySelector(".submit");
+const cancelBtn = document.querySelector(".cancel")
+const bookTitle = document.getElementById("book_title");
+const authorName = document.getElementById("author_name");
+const numPages = document.getElementById("num_pages");
+const finished = document.getElementById("finished");
 
-function bookFinished() {
-  const mark = document.getElementById("toggle");
-  const marked = mark.addEventListener("click", function () {
-    if (Element.includes("Unfinished")) {
-      replace("Unfinished", "Finished");
-    } else if (Element.includes("Finished")) {
-      replace("Finished", "Unfinished");
-    }
-  });
-}
-  */
+addBookBtn.addEventListener("click", () => {
+  modal.showModal();
+});
 
-// User needs to input book title, author, pages, and whether it's finished or not
 
-// all book objects stored in an array:
-const myLibrary = [];
-const finishedBtn = document.querySelector("#toggle");
-const addBookBtn = document.querySelector("#addBook");
+
+/*
 // object constructor:
-function Book(title, author, pages) {
+function Book(title, author, pages, finished) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.finished = finished;
 }
-// clicking on the ADD BOOK button needs to 1. show a modal 2. let the user type in title, author and pages 3. let the user select whether the book is finished or not 4. Let user either cancel or submit the info to myLibrary
-addBookBtn.onclick = () => addBookToLibrary();
+// how do I get this object into the library array?
+// how does it know  title is book-title on the card, and author is
 
-function addBookToLibrary() {}
+// clicking on the ADD BOOK button needs to 1. show a modal 2. let the user type in title, author and pages 3. let the user select whether the book is finished or not 4. Let user either cancel or submit the info to myLibrary
+
+document.getElementById("submit").onclick = function addBookToLibrary() {
+  return myLibrary.push().value;
+};
+
+addBookToLibrary([{ title: "The Bible" }, { author: "God" }, { pages: 500 }]);
+
+console.log(myLibrary); */
